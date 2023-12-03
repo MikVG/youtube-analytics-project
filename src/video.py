f2a1968b371a6b8a424aa0d95e41b063d484029b
Mikhail Glushkov <miggl@mail.ru>
2023-12-03 23:08:51 +0300

    HomeWork 16_1 Task 2

--- src/video.py
+++ src/video.py
@@ -13,15 +13,25 @@ class Video():
     def __init__(self, id_video):
         """
         Экземпляр инициализируется по id видео. Дальше все данные будут подтягиваться по API
+        Задание 6: инициализируется по id видео. Если пользователь передал id, с которым невозможно получить
+        данные о видео по API, то у экземпляра инициализируется только свойство video_id, а остальные поля
+        принимают значение None
         """
-        self.id_video = id_video
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=id_video
-                                               ).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.link = f'https://www.youtube.com/watch?v={self.id_video}'
-        self.viewCount = video_response['items'][0]['statistics']['viewCount']
-        self.likeCount = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.id_video = id_video
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=id_video
+                                                   ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.link = f'https://www.youtube.com/watch?v={self.id_video}'
+            self.viewCount = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.id_video = id_video
+            self.title = None
+            self.link = None
+            self.viewCount = None
+            self.like_count = None
 
     def __str__(self):
         """
